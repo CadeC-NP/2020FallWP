c7d77ce83c6ed1bc2b9011a101f75340e83be7fd
Cade Cabrera <cabreracade@gmail.com>
2020-10-14 19:04:50 -0400

    Dynamic Posts for Feed

---
+++ client/src/models/feed.js
@@ -0,0 +1,25 @@
+/*
+    Feed Model. The raw data for the feed display.
+    Currently mocked at the client side.
+*/
+export const posts = [
+    {
+        url: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-0/s600x600/36329355_10211525867333502_7504074251486363648_n.jpg?_nc_cat=100&_nc_sid=8bfeb9&_nc_ohc=QQTwIfQPAKAAX_ChAif&_nc_ht=scontent-lga3-1.xx&tp=7&oh=a891913e7c18e3a7d91f8cf2c2a1f95a&oe=5FAC311C',
+        message: '',
+        owner: {
+            name: 'Cade Cabrera',
+            handle: 'cabreracade',
+            profile: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/21370920_10209622095900406_1558557478066919662_n.jpg?_nc_cat=101&_nc_sid=730e14&_nc_ohc=SACDV3bx78gAX96KbJb&_nc_ht=scontent-lga3-1.xx&oh=47b81e9ad653b26796e4b0f8c5ccbb91&oe=5FACA43C'
+        }
+    },
+    {
+        url: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-0/p526x296/120529438_10217324855304577_2880916701251424792_o.jpg?_nc_cat=107&_nc_sid=84a396&_nc_ohc=4_R3vKU4n-UAX-0tekc&_nc_ht=scontent-lga3-1.xx&tp=6&oh=fe67f36bbe1fddd559eec619c268f294&oe=5FAC9F28',
+        message: '',
+        owner: {
+            name: 'Curt Cabrera',
+            handle: 'curtcabrera',
+            profile: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/21370920_10209622095900406_1558557478066919662_n.jpg?_nc_cat=101&_nc_sid=730e14&_nc_ohc=SACDV3bx78gAX96KbJb&_nc_ht=scontent-lga3-1.xx&oh=47b81e9ad653b26796e4b0f8c5ccbb91&oe=5FACA43C'
+        }
+    },
+];
+
